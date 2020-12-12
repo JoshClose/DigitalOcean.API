ed65209ffdfc3abdeab042597acdd9ff7152ff90
Josh Close <narshe@gmail.com>
2020-12-12 13:28:32 -0600

    Completed container registry.

--- DigitalOcean.API/Models/Responses/ContainerRegistryRepository.cs
+++ DigitalOcean.API/Models/Responses/ContainerRegistryRepository.cs
@@ -4,15 +4,24 @@ using System.Diagnostics;
 namespace DigitalOcean.API.Models.Responses {
     [DebuggerDisplay("Name = {Name}, RegistryName = {Registryname}, TagCount = {TagCount}")]
     public class ContainerRegistryRepository {
-        [JsonProperty("registry_name")]
+        /// <summary>
+        /// A globally unique name for the container registry. Must be lowercase and be composed only of numbers, letters and "-", up to a limit of 63 characters.
+        /// </summary>
         public string RegistryName { get; set; }
 
+        /// <summary>
+        /// The name of the repository.
+        /// </summary>
         public string Name { get; set; }
 
-        [JsonProperty("tag_count")]
-        public int TagCount { get; set; }
-
-        [JsonProperty("latest_tag")]
+        /// <summary>
+        /// The latest tag of the repository.
+        /// </summary>
         public ContainerRegistryTag LatestTag { get; set; }
+
+        /// <summary>
+        /// The number of tags in the repository.
+        /// </summary>
+        public int TagCount { get; set; }
     }
 }
--- DigitalOcean.API/Models/Responses/ContainerRegistryTag.cs
+++ DigitalOcean.API/Models/Responses/ContainerRegistryTag.cs
@@ -5,23 +5,39 @@ using System.Diagnostics;
 namespace DigitalOcean.API.Models.Responses {
     [DebuggerDisplay("Tag = {Tag}, Repository = {Repository}, RegistryName = {RegistryName}")]
     public class ContainerRegistryTag {
-        [JsonProperty("registry_name")]
+        /// <summary>
+        /// The name of the container registry.
+        /// </summary>
         public string RegistryName { get; set; }
 
-        public string Repository { get; set; }
+        /// <summary>
+        /// The name of the repository.
+        /// </summary>
+        public string RepositoryName { get; set; }
 
+        /// <summary>
+        /// The name of the tag.
+        /// </summary>
         public string Tag { get; set; }
 
-        [JsonProperty("manifest_digest")]
+        /// <summary>
+        /// The digest of the manifest associated with the tag.
+        /// </summary>
         public string ManifestDigest { get; set; }
 
-        [JsonProperty("compressed_size_bytes")]
+        /// <summary>
+        /// The compressed size of the tag in bytes.
+        /// </summary>
         public int CompressedSizeBytes { get; set; }
 
-        [JsonProperty("size_bytes")]
+        /// <summary>
+        /// The uncompressed size of the tag in bytes (this size is calculated asynchronously so it may not be immediately available).
+        /// </summary>
         public int SizeBytes { get; set; }
 
-        [JsonProperty("updated_at")]
+        /// <summary>
+        /// The time the tag was last updated.
+        /// </summary>
         public DateTimeOffset UpdatedAt { get; set; }
     }
 }
